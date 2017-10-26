c392db00dec18cd6ee384677270bdb3c5dede38c
bdoring <doringbarbara@gmail.com>
2017-10-26 14:53:14 -0700

    user stories 4 and 5

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,37 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/pokemon', function(req, res, next) {
-  res.render('pokemon/index', { passedInData: 'Express' });
+  knex('trainers')
+    .then((trainerList) => {
+      knex('pokemon')
+        .select('pokemon.name AS pokemonName', 'pokemon.cp', 'pokemon.in_gym', 'trainers.name AS trainerName')
+        .join('trainers', 'trainers.id', 'pokemon.trainer_id')
+        .orderBy('pokemon.id', 'ASC')
+        .then((pokemonList) => {
+          res.render('pokemon/index', { trainers: trainerList, pokemon: pokemonList });
+        })
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})
+
+//CREATES NEW POKEMON
+router.post('/pokemon', function(req, res, next) {
+  let newPokemon = {
+    name: req.body.name,
+    trainer_id: req.body.trainer_id,
+    cp: req.body.cp,
+    in_gym: false
+  }
+  knex('pokemon')
+    .insert(newPokemon)
+    .then(() => {
+      res.redirect('/pokemon');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 })
 
 module.exports = router;
