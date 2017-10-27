73471ae95e2b1e75ae34a24a997a03cac34218e8
bdoring <doringbarbara@gmail.com>
2017-10-27 12:02:15 -0700

    trainers story 1

--- controllers/trainers.js
+++ controllers/trainers.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var router = express.Router();
+var knex = require('../db/knex.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('trainers/index', {passedInData: "xyz"});
+  knex('trainers')
+    .then((trainersList) => {
+      res.render('trainers/index', {trainers: trainersList});
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
 });
 
 module.exports = router;
